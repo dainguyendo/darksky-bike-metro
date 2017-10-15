609c49d5b9a180f518bb2d64d2222d2a36117844
dainguyendo <dainguyendo@gmail.com>
2017-10-15 15:04:32 -0400

    displaying save state via localstorage example

--- app/js/utils/utilities.js
+++ app/js/utils/utilities.js
@@ -99,3 +99,30 @@ exports.isEmpty = (object) => {
   }
   return true;
 };
+
+// Load state via local storage
+exports.loadState = () => {
+  console.log('Loading local storage state...');
+  try {
+    const serializedState = localStorage.getItem('state');
+    // If the state is null - return undefined to let Redux reducers provide inital state
+    if (serializedState === null) { return undefined; }
+    //  Else, parse the state
+    return JSON.parse(serializedState);
+  } catch (e) {
+    //  If there were any errors, return undefined like above
+    return undefined;
+  }
+};
+
+// Save state - not exactly a loader but relates to loadState
+exports.saveState = (state) => {
+  console.log('Saving state...');
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+    // Prevent application from crashing from trying to save state
+    console.log(`Error saving state: ${e}`);
+  }
+};
